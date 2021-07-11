d146519bdc2fe9d626b3e7043d09e6e7fc3c8e9e
Ryan Graff <ryantgraff@resistdesign.com>
2021-07-10 20:44:39 -0400

    feat: (index) Decouple reducers from parsing.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { CloudFormationResourceSpecificationData } from './CloudFormationResourceSpecification';
-import { CFResourceTypeMap } from './Types';
+import { CFResourceTypeMap, CFResourceTypePropertyMap } from './Types';
 
 type PropertyTypeMap = {
   [PropertyName: string]: string | undefined;
@@ -15,42 +15,48 @@ const Combo: CFResourceTypeMap = {
   ...PropertyTypes,
   ...ResourceTypes,
 };
+const GET_PROP_REDUCER =
+  (t: string, propsObj: CFResourceTypePropertyMap) =>
+  (acc: PropertyTypeMap, p: string) => {
+    const { Type, PrimitiveType, ItemType, PrimitiveItemType } = propsObj[p];
+    const typeDomain = t.split('.')[0] || '';
+    const itemTypePrefix =
+      ItemType && ItemType.indexOf('.') === -1 ? `${typeDomain}.` : '';
+    const mainTypePrefix =
+      Type && Type.indexOf('.') === -1 && Type !== 'List' && Type !== 'Map'
+        ? `${typeDomain}.`
+        : '';
+    const itemType = ItemType
+      ? `${itemTypePrefix}${ItemType}`
+      : PrimitiveItemType;
+    const mainType = Type ? `${mainTypePrefix}${Type}` : PrimitiveType;
+    const fullType =
+      itemType && mainType ? `${mainType}<${itemType}>` : itemType || mainType;
+    const typeIsReferenced = itemType
+      ? itemType in Combo
+      : mainType && mainType in Combo;
+
+    if (Type && !(mainType && mainType in Combo)) {
+      TypeKindsMap[Type] = true;
+    }
+
+    acc[p] = `${fullType || ''}: ${typeIsReferenced}`;
+
+    return acc;
+  };
+const GET_RESOURCE_REDUCER =
+  (combo: CFResourceTypeMap) => (acc: ResourceTypeMap, t: string) => {
+    const propsObj = combo[t]?.Properties || {};
+
+    acc[t] = Object.keys(propsObj).reduce(
+      GET_PROP_REDUCER(t, propsObj),
+      {} as PropertyTypeMap
+    );
+
+    return acc;
+  };
 const ResourceTypeMapData: ResourceTypeMap = Object.keys(Combo).reduce(
-  (acc1, t) => {
-    const propsObj = Combo[t]?.Properties || {};
-
-    acc1[t] = Object.keys(propsObj).reduce((acc2, p) => {
-      const { Type, PrimitiveType, ItemType, PrimitiveItemType } = propsObj[p];
-      const typeDomain = t.split('.')[0] || '';
-      const itemTypePrefix =
-        ItemType && ItemType.indexOf('.') === -1 ? `${typeDomain}.` : '';
-      const mainTypePrefix =
-        Type && Type.indexOf('.') === -1 && Type !== 'List' && Type !== 'Map'
-          ? `${typeDomain}.`
-          : '';
-      const itemType = ItemType
-        ? `${itemTypePrefix}${ItemType}`
-        : PrimitiveItemType;
-      const mainType = Type ? `${mainTypePrefix}${Type}` : PrimitiveType;
-      const fullType =
-        itemType && mainType
-          ? `${mainType}<${itemType}>`
-          : itemType || mainType;
-      const typeIsReferenced = itemType
-        ? itemType in Combo
-        : mainType && mainType in Combo;
-
-      if (Type && !(mainType && mainType in Combo)) {
-        TypeKindsMap[Type] = true;
-      }
-
-      acc2[p] = `${fullType}: ${typeIsReferenced}`;
-
-      return acc2;
-    }, {} as PropertyTypeMap);
-
-    return acc1;
-  },
+  GET_RESOURCE_REDUCER(Combo),
   {} as ResourceTypeMap
 );
 
