ac510818370eb3fa38450caac784c7f3a8287a67
Ryan Graff <ryantgraff@resistdesign.com>
2021-07-28 21:56:07 -0400

    feat: (repo) Add the basis for the utils workspace.

---
+++ workspaces/utils/src/index.ts
@@ -0,0 +1,58 @@
+import { CloudFormationParameter, CloudFormationTemplate } from '@aws-cf-builder/types';
+
+export type ParameterInfo = {
+  ParameterId: string;
+  Parameter: CloudFormationParameter;
+  Label: string;
+  Group?: string;
+};
+
+export const addParameter = (parameterInfo: ParameterInfo, template: CloudFormationTemplate) => {
+  const { ParameterId, Parameter = {}, Label, Group } = parameterInfo;
+  const { Parameters = {}, Metadata: { 'AWS::CloudFormation::Interface': { ParameterGroups = [], ParameterLabels = {} } = {} } = {} } = template;
+
+  let NewParameterGroups = ParameterGroups;
+
+  if (Group) {
+    const GroupObject = ParameterGroups.filter((g) => g.Label === Group)[0];
+
+    NewParameterGroups = GroupObject
+      ? ParameterGroups.map((g) =>
+          g.Label === Group
+            ? {
+                ...g,
+                Parameters: [...(g.Parameters || []), ParameterId],
+              }
+            : g
+        )
+      : [
+          {
+            Label: {
+              default: Group,
+            },
+            Parameters: [ParameterId],
+          },
+        ];
+  }
+
+  return {
+    ...template,
+    Parameters: {
+      ...Parameters,
+      [ParameterId]: Parameter,
+    },
+    Metadata: {
+      ...template.Metadata,
+      'AWS::CloudFormation::Interface': {
+        ...template?.Metadata?.['AWS::CloudFormation::Interface'],
+        ParameterGroups: NewParameterGroups,
+        ParameterLabels: {
+          ...ParameterLabels,
+          [ParameterId]: {
+            default: Label,
+          },
+        },
+      },
+    },
+  };
+};
