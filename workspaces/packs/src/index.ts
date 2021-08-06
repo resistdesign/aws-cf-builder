2ef0b5904c5f9c5a648a7053d889256851d1a7ec
Ryan Graff <ryantgraff@resistdesign.com>
2021-08-06 17:34:53 -0400

    feat: (packs secureFileStorage) Add options for setting bucket name.

--- workspaces/packs/src/index.ts
+++ workspaces/packs/src/index.ts
@@ -1,76 +1,92 @@
 import { addParameter, createResourcePack } from '@aws-cf-builder/utils';
 import { AWS, CloudFormationTemplate } from '@aws-cf-builder/types';
 
+/**
+ * Add a secure S3 Bucket with an optional parameter to set the bucket name.
+ * */
 export const secureFileStorage = createResourcePack(
   ({
     id,
+    bucketName = false,
     label,
+    group,
     delete: shouldDelete = true,
     blockPublicAccess = true,
     cors = false,
   }: {
     id: string;
-    label: string;
+    bucketName?: AWS.S3.Bucket['Properties']['BucketName'] | boolean;
+    label?: string;
+    group?: string;
     delete?: boolean;
     blockPublicAccess?: boolean;
     cors?: AWS.S3.Bucket['Properties']['CorsConfiguration'] | boolean;
   }) => {
+    const cleanLabel = !!label ? label : id;
     const ParameterId = `${id}Name`;
-
-    return addParameter(
-      {
-        ParameterId,
-        Group: `${label} Parameters`,
-        Label: `${label} Name`,
-        Parameter: {
-          Type: 'String',
-          Description: 'The name of the file storage resource.',
-        },
-      },
-      {
-        Resources: {
-          [id]: {
-            Type: 'AWS::S3::Bucket',
-            DeletionPolicy: shouldDelete ? 'Delete' : 'Retain',
-            Properties: {
-              BucketName: {
-                Ref: ParameterId,
-              },
-              CorsConfiguration:
-                typeof cors === 'object'
-                  ? cors
-                  : cors === true
-                  ? {
-                      CorsRules: [
-                        {
-                          AllowedHeaders: ['*'],
-                          AllowedMethods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD'],
-                          AllowedOrigins: ['*'],
-                        },
-                      ],
-                    }
-                  : undefined,
-              BucketEncryption: {
-                ServerSideEncryptionConfiguration: [
-                  {
-                    ServerSideEncryptionByDefault: {
-                      SSEAlgorithm: 'AES256',
-                    },
-                  },
-                ],
-              },
-              PublicAccessBlockConfiguration: blockPublicAccess
+    const stackPatch = {
+      Resources: {
+        [id]: {
+          Type: 'AWS::S3::Bucket',
+          DeletionPolicy: shouldDelete ? 'Delete' : 'Retain',
+          Properties: {
+            BucketName:
+              bucketName === true
                 ? {
-                    BlockPublicAcls: true,
-                    BlockPublicPolicy: true,
-                    IgnorePublicAcls: true,
-                    RestrictPublicBuckets: true,
+                    Ref: ParameterId,
+                  }
+                : bucketName === false
+                ? undefined
+                : bucketName,
+            CorsConfiguration:
+              typeof cors === 'object'
+                ? cors
+                : cors === true
+                ? {
+                    CorsRules: [
+                      {
+                        AllowedHeaders: ['*'],
+                        AllowedMethods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD'],
+                        AllowedOrigins: ['*'],
+                      },
+                    ],
                   }
                 : undefined,
+            BucketEncryption: {
+              ServerSideEncryptionConfiguration: [
+                {
+                  ServerSideEncryptionByDefault: {
+                    SSEAlgorithm: 'AES256',
+                  },
+                },
+              ],
+            },
+            PublicAccessBlockConfiguration: blockPublicAccess
+              ? {
+                  BlockPublicAcls: true,
+                  BlockPublicPolicy: true,
+                  IgnorePublicAcls: true,
+                  RestrictPublicBuckets: true,
+                }
+              : undefined,
+          },
+        },
+      } as CloudFormationTemplate['Resources'],
+    };
+
+    return bucketName === true
+      ? addParameter(
+          {
+            ParameterId,
+            Group: !!group ? group : `${cleanLabel} Parameters`,
+            Label: `${cleanLabel} Name`,
+            Parameter: {
+              Type: 'String',
+              Description: 'The name of the file storage resource.',
             },
           },
-        } as CloudFormationTemplate['Resources'],
-      } as any
-    );
+          stackPatch as any
+        )
+      : stackPatch;
   }
 );
