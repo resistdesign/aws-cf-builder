55ae022dea9a405c39c635c6ce56569695234450
Ryan Graff <ryantgraff@resistdesign.com>
2021-08-17 08:23:18 -0400

    feat: (packs) Add the basis for cloud-function.

---
+++ workspaces/packs/src/cloud-function.ts
@@ -0,0 +1,74 @@
+import { AWS } from '@aws-cf-builder/types';
+
+export const PLACEHOLDER_FUNCTION_CODE: AWS.Lambda.Function.Code = {
+  ZipFile:
+    "module.exports = {handler: async () => ({\n            statusCode: 200,\n            headers: {'Content-Type': 'application/json'},\n            body: '\"You did it!\"'\n          })};\n",
+};
+
+export type AddCloudFunctionConfig = {
+  id: string;
+  code?: AWS.Lambda.Function.Code;
+};
+
+export const addCloudFunction = ({ id, code = PLACEHOLDER_FUNCTION_CODE }) => {
+  return {
+    Resources: {
+      APICloudFunctionRole: {
+        Type: 'AWS::IAM::Role',
+        Properties: {
+          ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'],
+          AssumeRolePolicyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Action: ['sts:AssumeRole'],
+                Effect: 'Allow',
+                Principal: {
+                  Service: ['lambda.amazonaws.com'],
+                },
+              },
+            ],
+          },
+          Policies: [
+            {
+              PolicyName: 'lambda-parameter-store',
+              PolicyDocument: {
+                Statement: [
+                  {
+                    Effect: 'Allow',
+                    Action: ['ssm:GetParameter*'],
+                    // Fix this!
+                    Resource: null,
+                  },
+                ],
+                Version: '2012-10-17T00:00:00.000Z',
+              },
+            },
+          ],
+        },
+      },
+      APICloudFunction: {
+        Type: 'AWS::Lambda::Function',
+        Properties: {
+          Timeout: 30,
+          Code: code,
+          Environment: {
+            Variables: {
+              // Fix this!
+              NODE_ENV: null,
+              // Fix this!
+              CLIENT_ORIGIN: null,
+              // Fix this!
+              API_CONFIG_PARAMETER_PATH: null,
+            },
+          },
+          Handler: 'index.handler',
+          Role: {
+            'Fn::GetAtt': ['APICloudFunctionRole', 'Arn'],
+          },
+          Runtime: 'nodejs12.x',
+        },
+      },
+    },
+  };
+};
