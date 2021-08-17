2bcad73b1ad4011ad3a6d0cff613e7b1177e5ca6
Ryan Graff <ryantgraff@resistdesign.com>
2021-08-17 08:46:09 -0400

    test: (packs cloud-function) Add a test.

--- workspaces/packs/src/cloud-function.ts
+++ workspaces/packs/src/cloud-function.ts
@@ -1,4 +1,5 @@
 import { AWS } from '@aws-cf-builder/types';
+import { createResourcePack } from '@aws-cf-builder/utils';
 
 export const PLACEHOLDER_FUNCTION_CODE: AWS.Lambda.Function.Code = {
   ZipFile:
@@ -15,68 +16,70 @@ export type AddCloudFunctionConfig = {
   policies?: AWS.IAM.Role.Policy[];
 };
 
-export const addCloudFunction = ({
-  id,
-  code = PLACEHOLDER_FUNCTION_CODE,
-  environment = {
-    Variables: {
-      NODE_ENV: 'production',
-    },
-  },
-  handler = 'index.handler',
-  runtime = 'nodejs12.x',
-  timeout = 30,
-  policies = [
-    {
-      PolicyName: 'lambda-parameter-store',
-      PolicyDocument: {
-        Statement: [
-          // By default, the lambda can do anything.
-          {
-            Effect: 'Allow',
-            Action: ['*'],
-            Resource: '*',
-          },
-        ],
-        Version: '2012-10-17T00:00:00.000Z',
+export const addCloudFunction = createResourcePack(
+  ({
+    id,
+    code = PLACEHOLDER_FUNCTION_CODE,
+    environment = {
+      Variables: {
+        NODE_ENV: 'production',
       },
     },
-  ],
-}: AddCloudFunctionConfig) => {
-  return {
-    Resources: {
-      [`${id}CloudFunctionRole`]: {
-        Type: 'AWS::IAM::Role',
-        Properties: {
-          ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'],
-          AssumeRolePolicyDocument: {
-            Version: '2012-10-17',
-            Statement: [
-              {
-                Action: ['sts:AssumeRole'],
-                Effect: 'Allow',
-                Principal: {
-                  Service: ['lambda.amazonaws.com'],
+    handler = 'index.handler',
+    runtime = 'nodejs12.x',
+    timeout = 30,
+    policies = [
+      {
+        PolicyName: 'lambda-parameter-store',
+        PolicyDocument: {
+          Statement: [
+            // By default, the lambda can do anything.
+            {
+              Effect: 'Allow',
+              Action: ['*'],
+              Resource: '*',
+            },
+          ],
+          Version: '2012-10-17T00:00:00.000Z',
+        },
+      },
+    ],
+  }: AddCloudFunctionConfig) => {
+    return {
+      Resources: {
+        [`${id}CloudFunctionRole`]: {
+          Type: 'AWS::IAM::Role',
+          Properties: {
+            ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'],
+            AssumeRolePolicyDocument: {
+              Version: '2012-10-17',
+              Statement: [
+                {
+                  Action: ['sts:AssumeRole'],
+                  Effect: 'Allow',
+                  Principal: {
+                    Service: ['lambda.amazonaws.com'],
+                  },
                 },
-              },
-            ],
+              ],
+            },
+            Policies: policies,
           },
-          Policies: policies,
+        },
+        [`${id}CloudFunction`]: {
+          Type: 'AWS::Lambda::Function',
+          Properties: {
+            Timeout: timeout,
+            Code: code,
+            Environment: environment,
+            Handler: handler,
+            Role: {
+              'Fn::GetAtt': [`${id}CloudFunctionRole`, 'Arn'],
+            },
+            Runtime: runtime,
+          },
         },
       },
-      [`${id}CloudFunction`]: {
-        Type: 'AWS::Lambda::Function',
-        Properties: {
-          Timeout: timeout,
-          Code: code,
-          Environment: environment,
-          Handler: handler,
-          Role: {
-            'Fn::GetAtt': [`${id}CloudFunctionRole`, 'Arn'],
-          },
-          Runtime: runtime,
-        },
-      },
-    },
-  };
-};
+    };
+  }
+);
