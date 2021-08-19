831e5fcef21de6bf944eeae02f109ebce44c6c53
Ryan Graff <ryantgraff@resistdesign.com>
2021-08-18 23:45:00 -0400

    feat: (packs cloud-function addCloudFunction) Add runtime type options.

--- workspaces/packs/src/cloud-function.ts
+++ workspaces/packs/src/cloud-function.ts
@@ -11,7 +11,32 @@ export type AddCloudFunctionConfig = {
   code?: AWS.Lambda.Function.Code;
   environment?: AWS.Lambda.Function.Environment;
   handler?: any;
-  runtime?: any;
+  runtime?:
+    | 'dotnetcore1.0'
+    | 'dotnetcore2.0'
+    | 'dotnetcore2.1'
+    | 'dotnetcore3.1'
+    | 'go1.x'
+    | 'java11'
+    | 'java8'
+    | 'java8.al2'
+    | 'nodejs'
+    | 'nodejs10.x'
+    | 'nodejs12.x'
+    | 'nodejs14.x'
+    | 'nodejs4.3'
+    | 'nodejs4.3-edge'
+    | 'nodejs6.10'
+    | 'nodejs8.10'
+    | 'provided'
+    | 'provided.al2'
+    | 'python2.7'
+    | 'python3.6'
+    | 'python3.7'
+    | 'python3.8'
+    | 'python3.9'
+    | 'ruby2.5'
+    | 'ruby2.7';
   timeout?: any;
   policies?: AWS.IAM.Role.Policy[];
 };
