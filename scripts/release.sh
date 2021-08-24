c9f2c7049cd9e7def97aa30bf3f9e7afa32c33f5
Ryan Graff <ryantgraff@resistdesign.com>
2021-08-23 22:56:48 -0400

    feat: (repo release) Add git commands.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Get release type.
 read -p 'Type (major|minor|patch): ' TYPE
 
+# Update workspace package versions, build, install and publish.
 yarn workspaces foreach version $TYPE
 yarn
 yarn build
 yarn
 yarn workspaces foreach npm publish
+
+# Commit and push version updates.
+git add .
+git commit -m "chore: (repo) Release a ${TYPE} version."
+git push
+clear
+git status
