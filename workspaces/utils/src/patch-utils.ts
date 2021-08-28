eaeab11f2b3f4b60c25bcf4637acf21cee6c95f2
Ryan Graff <ryantgraff@resistdesign.com>
2021-08-28 18:57:29 -0400

    feat: (utils patch-utils mergeValues) Add accumulate-unique and accumulate-unique-by merge strategies.

--- workspaces/utils/src/patch-utils.ts
+++ workspaces/utils/src/patch-utils.ts
@@ -1,10 +1,15 @@
-export type MergeStrategy = 'transpose' | 'accumulate' | 'replace';
+export type MergeStrategy = 'transpose' | 'accumulate' | 'accumulate-unique' | 'accumulate-unique-by' | 'replace';
+
+export type MergeStrategyDescriptor = {
+  strategy: MergeStrategy;
+  data?: any;
+};
 
 export type ValuePathString = string;
 
 export type ValuePathArray = (string | number)[];
 
-export type MergeStrategyMap = Record<ValuePathString, MergeStrategy>;
+export type MergeStrategyMap = Record<ValuePathString, MergeStrategyDescriptor>;
 
 export const DEFAULT_MERGE_STRATEGY: MergeStrategy = 'transpose';
 
@@ -20,8 +25,14 @@ export const mergeValues = (valuePathArray: ValuePathArray = [], existingValue: 
   const arrayIndexWildcardValuePathString = getValuePathString(valuePathArray.map((p, i) => (i === valuePathArray.length - 1 ? '#' : p)));
   const lastValuePathPart = valuePathArray[valuePathArray.length - 1];
   const {
-    [valuePathString]: specificKeyMergeStrategy = DEFAULT_MERGE_STRATEGY,
-    [arrayIndexWildcardValuePathString]: arrayIndexWildcardMergeStrategy = DEFAULT_MERGE_STRATEGY,
+    [valuePathString]: {
+      strategy: specificKeyMergeStrategy = DEFAULT_MERGE_STRATEGY,
+      data: specificKeyMergeStrategyData = undefined,
+    } = {} as Partial<MergeStrategyDescriptor>,
+    [arrayIndexWildcardValuePathString]: {
+      strategy: arrayIndexWildcardMergeStrategy = DEFAULT_MERGE_STRATEGY,
+      data: arrayIndexWildcardMergeStrategyData = undefined,
+    } = {} as Partial<MergeStrategyDescriptor>,
   } = mergeStrategyMap;
   const mergeStrategy =
     valuePathString in mergeStrategyMap
@@ -29,13 +40,52 @@ export const mergeValues = (valuePathArray: ValuePathArray = [], existingValue: 
       : typeof lastValuePathPart === 'number'
       ? arrayIndexWildcardMergeStrategy
       : specificKeyMergeStrategy;
+  const mergeStrategyData =
+    valuePathString in mergeStrategyMap
+      ? specificKeyMergeStrategyData
+      : typeof lastValuePathPart === 'number'
+      ? arrayIndexWildcardMergeStrategyData
+      : specificKeyMergeStrategyData;
 
   if (mergeStrategy !== 'replace') {
     if (isConstructedFrom(existingValue, Array) && isConstructedFrom(newValue, Array)) {
       if (mergeStrategy === 'accumulate') {
         return [...existingValue, ...newValue];
+      } else if (mergeStrategy === 'accumulate-unique') {
+        return [...existingValue, ...newValue.filter((item: any) => existingValue.indexOf(item) === -1)];
+      } else if (mergeStrategy === 'accumulate-unique-by') {
+        const keyName = mergeStrategyData as string;
+        const existingItemMap: Record<string, any> = {};
+        const newItemMap: Record<string, any> = {};
+
+        for (let i = 0; i < existingValue.length; i++) {
+          const existingItem = existingValue[i];
+
+          if (existingItem && typeof existingItem === 'object') {
+            const { [keyName]: identifier = undefined } = existingItem;
+
+            existingItemMap[identifier as string] = existingItem;
+          }
+        }
+
+        for (let j = 0; j < newValue.length; j++) {
+          const newItem = existingValue[j];
+
+          if (newItem && typeof newItem === 'object') {
+            const { [keyName]: identifier = undefined } = newItem;
+
+            newItemMap[identifier as string] = newItem;
+          }
+        }
+
+        return Object.keys({
+          ...existingItemMap,
+          ...newItemMap,
+        }).forEach((id, index) => mergeValues([...valuePathArray, index], existingItemMap[id], newItemMap[id], mergeStrategyMap));
       } else if (mergeStrategy === 'transpose') {
-        return [...new Array(Math.max(existingValue.length, newValue.length))].map((_empty, index) =>
+        const fullLength = Math.max(existingValue.length, newValue.length);
+
+        return [...new Array(fullLength)].map((_empty, index) =>
           mergeValues([...valuePathArray, index], existingValue[index], newValue[index], mergeStrategyMap)
         );
       }
